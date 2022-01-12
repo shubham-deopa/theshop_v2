48ca980660d310c87420e8c32dd2f705c650d86f
Shubham Deopa <shubham.deopa1996@gmail.com>
2022-01-12 17:25:59 +0530

    till linkset

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
             <Route exact path="/storelaunch" component={Storelaunch} />
             <Route exact path="/events" component={Events} />
         </Switch>
-    <>
+    </>
     );
   }
 }
